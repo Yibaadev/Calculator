a1ea1c7d86f689f06bb9a42e34f1719335261737
Ulrich Ondoa <Yibaadev>
2026-06-19 14:17:31 +0100

    modifaication service

--- src/main/java/co/kozao/calculator/service/calculatorService.java
+++ src/main/java/co/kozao/calculator/service/calculatorService.java
@@ -1,5 +1,4 @@
 package co.kozao.calculator.service;
-
 import org.apache.commons.math3.util.CombinatoricsUtils;
 
 public class calculatorService {
@@ -8,43 +7,38 @@ public class calculatorService {
         return nbre1 + nbre2;
     }
 
+    public static double substraction(double nbre1, double nbre2) {
+        return nbre1 - nbre2;
+    }
 
-	public static double substraction(double nbre1, double nbre2) {
-		return nbre1 - nbre2;
-	}
+    public static double multiplication(double nbre1, double nbre2) {
+        return nbre1 * nbre2;
+    }
 
-	public static double multiplication(double nbre1, double nbre2) {
-		return nbre1 * nbre2;
-	}
+    public static String division(double nbre1, double nbre2) {
+        if (nbre2 == 0) {
+            return "Erreur : impossible de diviser par zéro";
+        } else {
+            return String.valueOf(nbre1 / nbre2);
+        }
+    }
 
-	public static double division(double nbre1, double nbre2) {
-		if (nbre2 == 0) {
-			throw new IllegalArgumentException("Erreur, impossible de diviser par zéro");
-		} else {
-			return nbre1 / nbre2;
-		}
-	}
+    public static double power(double nbre1, double nbre2) {
+        return Math.pow(nbre1, nbre2);
+    }
 
-	public static double power(double nbre1, double nbre2) {
-		return Math.pow(nbre1, nbre2);
-	}
+    public static String squaReoot(double nbre1) {
+        if (nbre1 < 0) {
+            return "Erreur : impossible de calculer la racine d'un nombre négatif";
+        } else {
+            return String.valueOf(Math.sqrt(nbre1));
+        }
+    }
 
-	public static double squaReoot(double nbre1) {
-		if (nbre1 < 0) {
-			throw new IllegalArgumentException("Erreur, impossible de faire la racine carré de d'un nombre négatif");
-		} else {
-			return Math.sqrt(nbre1);
-		}
-	}
-
-	public double factorial(int nbre1) {
-		if (nbre1 < 0) {
-			throw new IllegalArgumentException("Erreur : Factorielle d'un nombre négatif");
-		}
-		return CombinatoricsUtils.factorial(nbre1);
-
-	}
-
+    public static long factorial(int nbre1) {
+        if (nbre1 < 0) {
+            throw new IllegalArgumentException("Erreur : Factorielle d'un nombre négatif");
+        }
+        return CombinatoricsUtils.factorial(nbre1);
+    }
 }
-
-   
